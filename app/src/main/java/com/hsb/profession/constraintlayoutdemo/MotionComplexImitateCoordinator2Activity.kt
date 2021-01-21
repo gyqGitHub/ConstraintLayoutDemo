2c44f529dcc9440438b0c4219b6114af4b953066
guyuqiang <guyuqiang@huishoubao.com.cn>
2021-01-21 18:00:56 +0800

    解决滑动顶部布局出现跳动的情况

---
+++ app/src/main/java/com/hsb/profession/constraintlayoutdemo/MotionComplexImitateCoordinator2Activity.kt
@@ -0,0 +1,11 @@
+package com.hsb.profession.constraintlayoutdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionComplexImitateCoordinator2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_complex_imitate_coordinator2)
+    }
+}
